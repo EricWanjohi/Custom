1621aa4945d4cc35ad699d9b97647b4ef12dd2c2
Eric W <ericwanjohi.ew@gmail.com>
2020-03-16 21:17:21 +0300

    - Created and implemented Retrofit client instance class - Created @GET request for leagues data in SportDbApi interface class

--- app/src/main/java/ke/co/droidsense/custom/ApiInterface/SportsDbApi.java
+++ app/src/main/java/ke/co/droidsense/custom/ApiInterface/SportsDbApi.java
@@ -1,6 +1,16 @@
 package ke.co.droidsense.custom.ApiInterface;
 
-public interface SportsDbApi {
+import java.util.List;
 
+import ke.co.droidsense.custom.models.Leagues;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface SportsDbApi {
+    //Create Retrofit Call methods.
+
+    //Get All Leagues.
+    @GET("all_leagues.php")
+    Call<List<Leagues>> getLeagues();
 
 }
---
+++ app/src/main/java/ke/co/droidsense/custom/utils/RetrofitClient.java
@@ -0,0 +1,24 @@
+package ke.co.droidsense.custom.utils;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    //Create Retrofit Instance.
+    private static Retrofit retrofit;
+    private static String Base_Url = "https://www.thesportsdb.com/api/v1/json/1/";
+
+    //Get Retrofit Instance.
+    public static Retrofit getRetrofit() {
+        //Check if instance is null.
+        if (retrofit == null) {
+            //Create new Instance.
+            retrofit = new Retrofit.Builder()
+                    .baseUrl( Base_Url )
+                    .addConverterFactory( GsonConverterFactory.create() )
+                    .build();
+        }
+        //Else return instance.
+        return retrofit;
+    }
+}
